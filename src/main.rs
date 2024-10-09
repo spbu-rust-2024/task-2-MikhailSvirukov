f2aa8e3405e63fc7dd1ba24cea0fd3d47fff92bd
Mikhail <mihailsvirukov@mail.ru>
2024-10-09 20:03:00 +0300

    +cycle for even variants

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,18 @@ fn main() {
     io::stdin()
         .read_line(&mut line)
         .expect("impossible");
+    print!("{}", line);
     let vector = line.as_bytes();
     let mut start=0;
-    let mut finish=vector.len();
+    let mut finish=0;
     let mut maximum=1;
 
     for i in 1..(vector.len())/2 {
         let mut current=1;
         let mut cur_start=i;
         let mut cur_finish=i;
-        for u in 1..i+1 {
-            if (vector[i-u]==vector[i+u]) {
+        for u in 1..i {
+            if vector[i-u]==vector[i+u] {
                 cur_finish=cur_finish+1;
                 cur_start=cur_start-1;
                 current=current+2;
@@ -23,8 +24,7 @@ fn main() {
                 break;
             }
         }
-        if current>maximum {maximum=current; start=cur_start; finish=cur_finish; println!("1");}
-        current=1;
+        if current>maximum {maximum=current; start=cur_start; finish=cur_finish;}
     }
 
     for i in (vector.len())/2..vector.len()-1 {
@@ -32,7 +32,7 @@ fn main() {
         let mut cur_start=i;
         let mut cur_finish=i;
         for u in 1..vector.len()-i {
-            if (vector[i-u]==vector[u+i]) {
+            if vector[i-u]==vector[u+i] {
                 cur_finish=cur_finish+1;
                 cur_start=cur_start-1;
                 current=current+2;
@@ -40,12 +40,11 @@ fn main() {
                 break;
             }
         }
-        if current>maximum {maximum=current; start=cur_start; finish=cur_finish;println!("2");}
-        current=1;
+        if current>maximum {maximum=current; start=cur_start; finish=cur_finish;}
     }
 
-    for i in 1..vector.len() {
-        let mut cur_start=i;
+    for i in 1..vector.len()/2 {
+        let mut cur_start=i-1;
         let mut cur_finish=i;
         let mut current=0;
         if vector[i]==vector[i-1] {
@@ -59,18 +58,18 @@ fn main() {
                     break;
                 }
             }
-            if current>maximum {maximum=current; start=cur_start; finish=cur_finish;println!("3");}
+            if current>maximum {maximum=current; start=cur_start; finish=cur_finish}
         }
     }
 
-    for i in vector.len()/2..vector.len() {
-        let mut cur_start=i;
+    for i in vector.len()/2..vector.len()-1 {
+        let mut cur_start=i-1;
         let mut cur_finish=i;
         let mut current=0;
         if vector[i]==vector[i-1] {
             current=current+2;
-            for u in 1..vector.len()-i {
-                if vector[i-u-1]==vector[i+u] {
+            for u in 1..vector.len()-i-1 {
+                if vector[i-1-u]==vector[i+u] {
                     current=current+2;
                     cur_finish=cur_finish+1;
                     cur_start=cur_start-1;
@@ -78,8 +77,10 @@ fn main() {
                     break;
                 }
             }
-            if current>maximum {maximum=current; start=cur_start; finish=cur_finish;println!("4");}
+            if current>maximum {maximum=current; start=cur_start; finish=cur_finish;}
         }
     }
-    print!("{} {} {}", maximum, start, finish);
+
+
+    println!("{}", &line[start..finish+1]);
 } 
