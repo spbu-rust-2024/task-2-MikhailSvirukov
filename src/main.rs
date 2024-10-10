e3c311eb2577d95d1b1a09699353d90d3da3cd05
Mikhail <mihailsvirukov@mail.ru>
2024-10-10 14:54:42 +0300

    func for even/uneven cycle + struct for maximum

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,86 @@
 use std::io;
 
-fn main() {
-    let mut line:String=String::new();
-    io::stdin()
-        .read_line(&mut line)
-        .expect("impossible");
-    let vector:Vec <u8>= line.chars().map(|x| x as u8).collect();
-    let length = vector.len();
-    let half = length/2;
-    let mut start=0;
-    let mut finish=0;
-    let mut maximum=1;
-
-
-    if length==1 {print!(""); return}
-
-    for i in 1..half {
-        let mut current=1;
-        let mut cur_start=i;
-        let mut cur_finish=i;
-        for u in 1..i+1 {
-            if vector[i-u]==vector[i+u] {
-                cur_finish=cur_finish+1;
-                cur_start=cur_start-1;
-                current=current+2;
-            }else {
-                break;
+struct Pol {
+    maximum: usize,
+    start: usize,
+    end: usize,
+}
+fn even(vector: &Vec<u8>) -> Pol {
+    let mut maximum = Pol {
+        maximum: 0,
+        start: 1,
+        end: 1,
+    };
+    for i in 1..vector.len() {
+        let mut current = Pol {
+            maximum: 0,
+            start: i - 1,
+            end: i,
+        };
+        if vector[i] == vector[i - 1] {
+            current.maximum += 2;
+            let mut u = 1;
+            while i - u > 0 && i + u < vector.len() {
+                if vector[i - u - 1] == vector[i + u] {
+                    current.end += 1;
+                    current.start -= 1;
+                    current.maximum += 2;
+                    u += 1;
+                } else {
+                    break;
+                }
             }
         }
-        if current>maximum {maximum=current; start=cur_start; finish=cur_finish;}
+        if current.maximum > maximum.maximum {
+            maximum = current
+        };
     }
-
-    for i in half..length {
-        let mut current=1;
-        let mut cur_start=i;
-        let mut cur_finish=i;
-        for u in 1..length-i {
-            if vector[i-u]==vector[u+i] {
-                cur_finish=cur_finish+1;
-                cur_start=cur_start-1;
-                current=current+2;
+    maximum
+}
+fn uneven(vector: &Vec<u8>) -> Pol {
+    let mut maximum = Pol {
+        maximum: 0,
+        start: 1,
+        end: 1,
+    };
+    for i in 1..vector.len() {
+        let mut current = Pol {
+            maximum: 0,
+            start: i,
+            end: i,
+        };
+        let mut u = 1;
+        while i - u > 0 && i + u < vector.len() {
+            if vector[i - u] == vector[i + u] {
+                current.end += 1;
+                current.start -= 1;
+                current.maximum += 2;
+                u += 1;
             } else {
                 break;
             }
         }
-        if current>maximum {maximum=current; start=cur_start; finish=cur_finish;}
+        if current.maximum > maximum.maximum {
+            maximum = current
+        };
     }
-
-    for i in 1..half {
-        let mut cur_start=i-1;
-        let mut cur_finish=i;
-        let mut current=0;
-        if vector[i]==vector[i-1] {
-            current=current+2;
-            for u in 1..i {
-                if vector[i-u-1]==vector[i+u] {
-                    cur_finish=cur_finish+1;
-                    cur_start=cur_start-1;
-                    current=current+2;
-                } else {
-                    break;
-                }
+    maximum
+}
+fn main() {
+    let mut line: String = String::new();
+    io::stdin().read_line(&mut line).expect("impossible");
+    let vector: Vec<u8> = line.chars().map(|x| x as u8).collect();
+    let even_pol = even(&vector);
+    let uneven_pol = uneven(&vector);
+    if line.len() > 1 {
+        if even_pol.maximum > uneven_pol.maximum {
+            for i in even_pol.start..even_pol.end+1 {
+                print!("{}", vector[i] as char);
             }
-            if current>maximum {maximum=current; start=cur_start; finish=cur_finish}
-        }
-    }
-
-    for i in half..length{
-        let mut cur_start=i-1;
-        let mut cur_finish=i;
-        let mut current=0;
-        if vector[i]==vector[i-1] {
-            current=current+2;
-            for u in 1..length-i-1 {
-                if vector[i-1-u]==vector[i+u] {
-                    current=current+2;
-                    cur_finish=cur_finish+1;
-                    cur_start=cur_start-1;
-                } else {
-                    break;
-                }
+        } else {
+            for i in uneven_pol.start..uneven_pol.end+1 {
+                print!("{}", vector[i] as char);
             }
-            if current>maximum {maximum=current; start=cur_start; finish=cur_finish;}
-        }
-    }
-
-    if length>1 {
-        for i in start..finish+1 {
-            print!("{}", vector[i] as char)
         }
     }
 }
